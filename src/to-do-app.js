74abe63f55b9af82721b574be7ff9d4ae71ceab7
ChantalDesrochers <chandesrochers@gmail.com>
2019-05-21 15:52:47 -0400

    experimenting w slots

--- src/to-do-app.js
+++ src/to-do-app.js
@@ -1,4 +1,5 @@
 const template = document.createElement('template');
+const templateTwo = document.createElement('template2');
 
 // template is cheaper than calling .innerHtml on all instances of the component
 template.innerHTML = `
@@ -19,12 +20,20 @@ template.innerHTML = `
     padding: 0;
     }
 </style>
+<slot></slot>
 <h1>To do</h1>
 
 <input type="text" placeholder="Add a new to do"></input>
 <button>✅</button>
+<p>Date</p>
+
 
 <ul id="todos"></ul>
+<slot></slot>
+`;
+
+templateTwo.innerHTML = `
+<h3>LALALALA</h3>
 `;
 
 class TodoApp extends HTMLElement {
@@ -33,11 +42,15 @@ class TodoApp extends HTMLElement {
         this._shadowRoot = this.attachShadow({ 'mode': 'open'});
         //cloning template to shadowroot
         this._shadowRoot.appendChild(template.content.cloneNode(true));
+        // this._shadowRoot.appendChild(templateTwo.content.cloneNode(true));
         this.$todoList = this._shadowRoot.querySelector('ul');
         this.$input = this._shadowRoot.querySelector('input');
 
+        console.log('slot query selector', this._shadowRoot.querySelector('slot'));
+        console.log('h1 query selector', this._shadowRoot.querySelector('h1'))
         this.$submitButton = this._shadowRoot.querySelector('button');
         this.$submitButton.addEventListener('click', this._addTodo.bind(this))
+       
         document.querySelector('to-do-app').todos = [];
     }
 
@@ -55,7 +68,8 @@ class TodoApp extends HTMLElement {
         console.log('input value', this.$input.value)
         console.log('todos', this._todos)
         if(this.$input.value.length > 0) {
-            this._todos.push({ text: this.$input.value, checked: false})
+            // this._todos.push({ text: this.$input.value, checked: false, date: 'today'})
+            this._todos.push({ text: this.$input.value, checked: false, date: new Date()})
             this._renderTodoList();
             this.$input.value = '';
         }
@@ -85,9 +99,12 @@ class TodoApp extends HTMLElement {
             }
 
             $todoItem.setAttribute('index', index);
+            $todoItem.setAttribute('date', todo.date)
 
             $todoItem.addEventListener('onRemove', this._removeTodo.bind(this));
             $todoItem.addEventListener('onToggle', this._toggleTodo.bind(this));
+
+            console.log('todo item in render todo list', $todoItem)
 
             this.$todoList.appendChild($todoItem);
         })
--- src/to-do-item.js
+++ src/to-do-item.js
@@ -19,10 +19,11 @@ template.innerHTML = `
     <input type="checkbox">
     <label></label>
     <button>❌</button>
+    <p></p>
 </li>
 `;
 
-class TodoItem extends  HTMLElement {
+class TodoItem extends HTMLElement {
     constructor() {
         super();
         this._shadowRoot = this.attachShadow({ 'mode': 'open'});
@@ -32,6 +33,10 @@ class TodoItem extends  HTMLElement {
         this.$removeButton = this._shadowRoot.querySelector('button');
         this.$checkbox = this._shadowRoot.querySelector('input');
         this.$text = this._shadowRoot.querySelector('label');
+        this.$date = this._shadowRoot.querySelector('p');
+
+        console.log('date in constructor', this.$date)
+        console.log('text in constructor', this.$text)
 
         this.$removeButton.addEventListener('click', (e) => {
             this.dispatchEvent(new CustomEvent('onRemove', {detail: this.index}));
@@ -42,17 +47,18 @@ class TodoItem extends  HTMLElement {
         });
     }
 
-    get checked() {
-        return this.hasAttribute('checked');
-    }
+    // get checked() {
+    //     return this.hasAttribute('checked');
+    // }
 
-    set checked(val) {
-        if (val) {
-            this.setAttribute('checked', '')
-        } else {
-            this.removeAttribute('checked');
-        }
-    }
+    // set checked(val) {
+    //     console.log('in setter', val)
+    //     if (val) {
+    //         this.setAttribute('checked', '')
+    //     } else {
+    //         this.removeAttribute('checked');
+    //     }
+    // }
 
     get index() {
         return this._index;
@@ -62,12 +68,26 @@ class TodoItem extends  HTMLElement {
         this.setAttribute('index', val);
     }
 
+    // get date() {
+    //     return this._date;
+    // }
+
+    // set date(val) {
+    //     console.log('in set date', val)
+    //     this.setAttribute('date', val)
+    // }
+
     connectedCallback() {
         console.log('inConnectedCallback')
         console.log('inConnectedCallback - this', this)
         if(!this.hasAttribute('text')) {
             // this.setAttribute('text', 'placeholder');
             this['text'] = 'placeholder';
+        }
+
+        if (!this.hasAttribute('date')) {
+            console.log('in connected callback...date')
+            this['date'] = 'today';
         }
 
         this._renderTodoItem();
@@ -83,16 +103,19 @@ class TodoItem extends  HTMLElement {
             this.$checkbox.removeAttribute('checked')
         }
         this.$text.innerHTML = this._text;
+        console.log('this.$date in render todo', this.$date)
+        console.log('this._date in render todo', this._date)
+        this.$date.innerHTML = this._date;
     }
 
     static get observedAttributes() {
-        return ['text', 'checked', 'index'];
+        return ['text', 'checked', 'index', 'date'];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
         console.log('oldValue', oldValue)
         console.log('name', name)
-        console.log('newValue', newValue)
+        console.log('newValue typeof', typeof newValue)
         switch(name){
             case 'text':
                 this._text = newValue;
@@ -103,6 +126,8 @@ class TodoItem extends  HTMLElement {
             case 'index':
                 this._index = parseInt(newValue);
                 break;
+            case 'date':
+                this._date = newValue;
         }
     }
 }
